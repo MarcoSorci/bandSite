c929dffb5f7676a7fe5bee982c409b6e19ec264c
JMIKEPAD <mchparodi@gmail.com>
2022-03-04 19:36:34 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { StoryComponent } from './components/wrapper/subComponents/story/story.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { StoryNavComponent } from './nav-menu/navContent/story-nav/story-nav.component';
+import { ShopComponent } from './nav-menu/navContent/shop/shop.component';
+import { GalleryComponent } from './nav-menu/navContent/gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    WrapperComponent
+    WrapperComponent,
+    StoryComponent,
+    NavMenuComponent,
+    StoryNavComponent,
+    ShopComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/nav-menu/navContent/story-nav/story-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-story-nav',
+  templateUrl: './story-nav.component.html',
+  styleUrls: ['./story-nav.component.scss']
+})
+export class StoryNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
